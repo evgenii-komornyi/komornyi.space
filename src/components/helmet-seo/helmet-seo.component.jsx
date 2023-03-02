7ad3495f89b91ae8b8764f6a6ce0019168becb23
evgenii-komornyi <evgeniikomornyi@gmail.com>
2023-03-02 13:35:16 +0200

    added react native skills an certificate

--- src/components/helmet-seo/helmet-seo.component.jsx
+++ src/components/helmet-seo/helmet-seo.component.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export const HelmetSeo = ({ title, description, url }) => {
+    const openGraph = `
+            <meta property="og:url" content=${url} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content=${title} />
+            <meta property="og:description" content=${description} />
+    `;
+
+    const headElement = document.querySelector('head');
+
+    console.log(headElement);
+
     return (
         <Helmet>
             <title>{title}</title>
             <meta name="description" content={description} />
-            <meta property="og:url" content={url} />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta
-                property="og:image"
-                content="https://media.kasperskydaily.com/wp-content/uploads/sites/92/2019/08/30094529/internet-in-space-featured.jpg"
-            />
-            <meta property="og:image:type" content="image/jpeg" />
-            <meta property="og:image:width" content="300" />
-            <meta property="og:image:height" content="300" />
+            <link rel="canonical" href={url} />
         </Helmet>
     );
 };
